720b0e7b3cc80b40f7ea02da589fb69a92fd568c
Colin Kelley <colindkelley@gmail.com>
2016-02-14 08:31:09 -0800

    Refactor to interpolate inside constants. And some general cleanup.

--- lib/add_magic_comment.rb
+++ lib/add_magic_comment.rb
@@ -1,47 +1,37 @@
 # frozen_string_literal: true
 
-# A simple library to prepend magic comments to multiple ".rb" files
+# A simple library to prepend magic comments to all Ruby files in a given folder
 
 module AddMagicComment
   MAGIC_COMMENT_PREFIX  = "frozen_string_literal"
   MAGIC_COMMENT_PATTERN = /^(-|(<%))?#\s*#{MAGIC_COMMENT_PREFIX}\s*(%>)?/m
   MAGIC_COMMENT         = "#{MAGIC_COMMENT_PREFIX}: true"
 
-  EXTENSIONS = {
-    "rb"   => "# {text}\n",
-    "rake" => "# {text}\n",
-    "haml" => "-# {text}\n",
-    "erb"  => "<%# {text} %>\n"
+  EXTENSION_COMMENTS = {
+    "rb"   => "# #{MAGIC_COMMENT}",
+    "rake" => "# #{MAGIC_COMMENT}",
+    "haml" => "-# #{MAGIC_COMMENT}",
+    "erb"  => "<%# #{MAGIC_COMMENT} %>"
   }
 
-  # Options :
-  # 1 : Encoding
-  # 2 : Path
-  # TODO : check that the encoding specified is a valid encoding
-  # TODO : allow use of only one option, so the encoding would be guessed (maybe using `file --mime`?)
-  def self.process(options)
-
-    directory = options[0] || Dir.pwd
-
-
-    # TODO : add options for recursivity (and application of the script to a single file)
+  def self.process(argv)
+    directory = argv.first || Dir.pwd
 
     count = 0
-    EXTENSIONS.each do |ext, comment_style|
-      frozen_literal_string_comment = comment_style.sub("{text}", MAGIC_COMMENT)
-      rbfiles = File.join(directory, "**", "*.#{ext}")
-      Dir.glob(rbfiles).each do |filename|
+    EXTENSION_COMMENTS.each do |ext, comment|
+      filename_pattern = File.join(directory, "**", "*.#{ext}")
+      Dir.glob(filename_pattern).each do |filename|
         next unless File.size(filename) > 0
         File.open(filename, "r+") do |file|
           lines = file.readlines
 
           # remove current encoding comment(s)
-          while lines[0] && lines[0].match(MAGIC_COMMENT_PATTERN)
+          while lines.first && lines.first.match(MAGIC_COMMENT_PATTERN)
             lines.shift
           end
 
           # set current encoding
-          lines.insert(0, frozen_literal_string_comment)
+          lines.insert(0, comment + "\n")
           count += 1
 
           file.pos = 0
