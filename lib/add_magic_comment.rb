4a67011692c919e57b44cdbf9c5e56839baa79ae
Zubin Henner <zubin@users.noreply.github.com>
2019-01-10 22:35:52 +1100

    Refactor to allow for additional config

--- lib/add_magic_comment.rb
+++ lib/add_magic_comment.rb
@@ -7,39 +7,38 @@ module AddMagicComment
   MAGIC_COMMENT_PATTERN = /^(-|(<%))?#\s*#{MAGIC_COMMENT_PREFIX}\s*(%>)?/
   MAGIC_COMMENT         = "#{MAGIC_COMMENT_PREFIX}: true"
 
-  EXTENSION_COMMENTS = {
-    "rb"   => "# #{MAGIC_COMMENT}",
-    "rake" => "# #{MAGIC_COMMENT}",
-    "rabl" => "# #{MAGIC_COMMENT}",
-    "jbuilder" => "# #{MAGIC_COMMENT}",
-    "haml" => "-# #{MAGIC_COMMENT}",
-    "slim" => "-# #{MAGIC_COMMENT}",
-    "erb"  => "<%# #{MAGIC_COMMENT} %>"
-  }
+  PATTERNS = [
+    {comment: "# #{MAGIC_COMMENT}", extnames: %w[rb rake rabl jbuilder]},
+    {comment: "-# #{MAGIC_COMMENT}", extnames: %w[haml slim]},
+    {comment: "<%# #{MAGIC_COMMENT} %>", extnames: %w[erb]},
+  ].freeze
 
   def self.process(argv)
     directory = argv.first || Dir.pwd
 
     count = 0
-    EXTENSION_COMMENTS.each do |ext, comment|
-      filename_pattern = File.join(directory, "**", "*.#{ext}")
-      Dir.glob(filename_pattern).each do |filename|
-        next unless File.size(filename) > 0
-        File.open(filename, "r+") do |file|
-          lines = file.readlines
+    PATTERNS.each do |pattern|
+      comment = pattern[:comment]
+      pattern[:extnames].each do |ext|
+        filename_pattern = File.join(directory, "**", "*.#{ext}")
+        Dir.glob(filename_pattern).each do |filename|
+          next unless File.size(filename) > 0
+          File.open(filename, "r+") do |file|
+            lines = file.readlines
 
-          # remove current encoding comment(s)
-          while lines.first && (lines.first.match(MAGIC_COMMENT_PATTERN) || lines.first.strip == '')
-            lines.shift
+            # remove current encoding comment(s)
+            while lines.first && (lines.first.match(MAGIC_COMMENT_PATTERN) || lines.first.strip == '')
+              lines.shift
+            end
+
+            # set current encoding
+            lines.insert(0, comment + "\n\n")
+            count += 1
+
+            file.pos = 0
+            file.puts(lines.join)
+            file.truncate(file.pos)
           end
-
-          # set current encoding
-          lines.insert(0, comment + "\n\n")
-          count += 1
-
-          file.pos = 0
-          file.puts(lines.join)
-          file.truncate(file.pos)
         end
       end
     end
